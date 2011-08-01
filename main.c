562915cd5b28c0aea507c9c118955a516a8d30c2
ginrou799 <ginrou799@gmail.com>
2011-08-01 10:46:38 +0900

    視差をPSFで表現する部分完了

--- main.c
+++ main.c
@@ -15,7 +15,8 @@
 
 int main(void)
 {
-  IMG* img = readImage( "img/MBP/screan.png" );
+  IMG* img = readImage( "img/MBP/center.png" );
+  IMG* gt  = readImage( "img/MBP/left.png" );
   Mat psf[MAX_DISPARITY];
 
   for( int d = 0; d < MAX_DISPARITY; ++d){
@@ -26,28 +27,44 @@ int main(void)
 	ELEM0( psf[d], h, w) = 0.0;
       }
     }
-    ELEM0( psf[d], 0, psf[d].clm/2 - d/2) = 1.0;
+    ELEM0( psf[d], 0, psf[d].clm/2 + d/2) = 1.0;
 
+    IMG* tmp = createImage( psf[d].row, psf[d].clm);
+    for(int h = 0; h < psf[d].row; ++h){
+      for(int w = 0; w < psf[d].clm; ++w){
+	IMG_ELEM(tmp, h, w) = ELEM0( psf[d], h, w);
+      }
+    }
+    char filename[256];
+    sprintf(filename, "img/MBP/psf%02d.png", d);
+    //    saveImage( tmp, filename);
+    
   }
 
-  IMG* map = readImage("img/MBP/disparityMap.png");
+  IMG* map = readImage("img/MBP/disparityMapLeft.png");
 
   convertScaleImage(map, map, 1.0/4.0, 0.0);
   
-  IMG* mapShift = createImage( map->width, map->height);
-  convertScaleImage(mapShift, mapShift, 0.0, MAX_DISPARITY);
-  for(int h = 0; h < map->height;++h){
-    for(int w = 0 ; w < map->width; ++w){
-      int disp = IMG_ELEM( map, h, w) ;
-      IMG_ELEM(mapShift, h, w + disp/2) = disp;
+  IMG* dst =  blurWithPSFMap( img, psf, map);
+  /*  
+  convertScaleImage( dst, dst, 0.0, 0.0);
+  for( int h = 0 ; h < dst->height; ++h){
+    for(int w = 0; w < dst->width; ++w){
+      int disp = IMG_ELEM( map, h, w);
+      IMG_ELEM( dst, h, w) = IMG_ELEM( img, h, w+disp/2);
     }
   }
-
-  IMG* dst =  blurWithPSFMap( img, psf, map);
+  */
+  IMG* dif = createImage( dst->height, dst->width );
+  for( int h = 0 ; h < dst->height; ++h){
+    for(int w = 0; w < dst->width; ++w){
+      IMG_ELEM( dif, h, w) = 15.0*abs(IMG_ELEM( dst, h, w) - IMG_ELEM(gt, h, w));
+    }
+  }
   
 
   saveImage( dst, "img/MBP/test.png");
-
+  saveImage( dif, "img/MBP/dif.png");
   return 0;
 
 }
