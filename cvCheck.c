8de8a139ef75c2f4b14d65c6f7d582dfcdc0fabc
ginrou799 <ginrou799@gmail.com>
2011-12-12 16:40:26 +0900

    Revert 3d88e492e7dc6c0470009a22bb0718e55e1044d6^..HEAD

--- cvCheck.c
+++ cvCheck.c
@@ -3,33 +3,8 @@
 #include <stdio.h>
 int main( int argc, char* argv[] ){
 
-  if( argc < 4 ){
-    printf("input arguments :\n");
-    printf("arg[1] : input left image\n");
-    printf("arg[2] : input right image\n");
-    printf("arg[3] : result disparity map name\n");
-  }
-
-  IplImage *imgLeft  = cvLoadImage( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-  IplImage *imgRight = cvLoadImage( argv[2], CV_LOAD_IMAGE_GRAYSCALE);
   
-  IplImage *dispMap;
-/*   CvStereoBMState* state = cvCreateStereoBMState( CV_STEREO_BM_BASIC, 128 ); */
-/*   dispMap = cvCreateImage( cvGetSize( imgLeft ), IPL_DEPTH_16S, 1); */
-/*   cvFindStereoCorrespondenceBM( imgLeft, imgRight, dispMap, state ); */
 
-  IplImage *dispLeft  = cvCreateImage( cvGetSize( imgLeft ), IPL_DEPTH_16S, 1 );
-  IplImage *dispRight = cvCreateImage( cvGetSize( imgLeft ), IPL_DEPTH_16S, 1 );
-  CvStereoGCState *gcState = cvCreateStereoGCState( 128, 100 );
-  cvFindStereoCorrespondenceGC( imgLeft, imgRight, dispLeft, dispRight, gcState, 0);
-  
-  dispMap = dispLeft;
-
-  IplImage *dst = cvCreateImage( cvGetSize( dispMap ), IPL_DEPTH_8U, 1);
-  cvConvertScale( dispMap, dst, 1.0 / 16.0, 0.0 );
-  cvSaveImage( argv[3], dst , NULL);
-
-  
 
   return 0;
 
